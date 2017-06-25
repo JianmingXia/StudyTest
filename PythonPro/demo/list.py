2cbd41b606e0115280dc316f6bcbbc65f87686c0
JianmingXia <1179801728@qq.com>
2017-06-25 22:57:20 +0800

    python study

---
+++ PythonPro/demo/list.py
@@ -0,0 +1,53 @@
+#coding=utf-8
+#列表 / 元组
+
+tup1 = ("all")
+print (tup1)
+
+tup1 = ("all",)
+print (tup1)
+
+# tup = ('physics', 'chemistry', 1997, 2000);
+#
+# print (tup);
+# del tup;
+# print ("After deleting tup : ")
+# # 空的元组输出会报错
+# print (tup);
+
+# tup1 = (12, 34.56);
+# tup2 = ('abc', 'xyz');
+#
+# # 以下修改元组元素操作是非法的。
+# # tup1[0] = 100;
+#
+# # 创建一个新的元组
+# tup3 = tup1 + tup2;
+# print (tup3);
+
+# tup1 = ('physics', 'chemistry', 1997, 2000);
+# tup2 = (1, 2, 3, 4, 5, 6, 7 );
+#
+# print ("tup1[0]: ", tup1[0])
+# print ("tup2[1:5]: ", tup2[1:5])
+
+# list1 = ['physics', 'chemistry', 1997, 2000];
+#
+# print (list1);
+# del list1[2];
+# print ("After deleting value at index 2 : ")
+# print (list1);
+
+# list = ['physics', 'chemistry', 1997, 2000];
+#
+# print ("Value available at index 2 : ")
+# print (list[2]);
+# list[2] = 2001;
+# print ("New value available at index 2 : ")
+# print (list[2]);
+
+# list1 = ['physics', 'chemistry', 1997, 2000];
+# list2 = [1, 2, 3, 4, 5, 6, 7 ];
+#
+# print ("list1[0]: ", list1[0])
+# print ("list2[1:5]: ", list2[1:5])
