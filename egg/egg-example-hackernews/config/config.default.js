56d1b9072ed56a33b9e29c20d37b2602c88022dc
Ryoma <xiajianming@quqi.com>
2018-07-19 09:52:48 +0800

    tpl render

--- egg/egg-example-hackernews/config/config.default.js
+++ egg/egg-example-hackernews/config/config.default.js
@@ -1,1 +1,9 @@
-exports.keys = 15197163397283911
+exports.keys = "15197163397283911";
+
+// 添加 view 配置
+exports.view = {
+    defaultViewEngine: 'nunjucks',
+    mapping: {
+        '.tpl': 'nunjucks',
+    },
+};
